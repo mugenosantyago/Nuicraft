7c6d1f6fef77a887dd51bf753bf310e8169392df
mugenosantyago <147428965+mugenosantyago@users.noreply.github.com>
2026-02-09 19:03:14 -0600

    f

--- src/main/java/eastonium/nuicraft/core/NuiCraftBlocks.java
+++ src/main/java/eastonium/nuicraft/core/NuiCraftBlocks.java
@@ -2,6 +2,9 @@ package eastonium.nuicraft.core;
 
 import eastonium.nuicraft.NuiCraft;
 import eastonium.nuicraft.block.*;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -11,79 +14,84 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 public class NuiCraftBlocks {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(NuiCraft.MODID);
 
+    /** Call on BlockBehaviour.Properties before registering (1.21 requires id set). */
+    private static BlockBehaviour.Properties withBlockId(String name, BlockBehaviour.Properties props) {
+        return props.setId(ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(NuiCraft.MODID, name)));
+    }
+
     // Fluid blocks - placeholder
     public static final DeferredBlock<Block> FLUID_PROTODERMIS = BLOCKS.registerSimpleBlock("fluid_protodermis",
-            BlockBehaviour.Properties.of().noCollission().strength(100.0F).noLootTable());
+            withBlockId("fluid_protodermis", BlockBehaviour.Properties.of().noCollission().strength(100.0F).noLootTable()));
     
     public static final DeferredBlock<Block> FLUID_PROTODERMIS_PURE = BLOCKS.registerSimpleBlock("fluid_protodermis_pure",
-            BlockBehaviour.Properties.of().noCollission().strength(100.0F).noLootTable());
+            withBlockId("fluid_protodermis_pure", BlockBehaviour.Properties.of().noCollission().strength(100.0F).noLootTable()));
     
     public static final DeferredBlock<Block> FLUID_PROTODERMIS_MOLTEN = BLOCKS.registerSimpleBlock("fluid_protodermis_molten",
-            BlockBehaviour.Properties.of().noCollission().strength(100.0F).noLootTable());
+            withBlockId("fluid_protodermis_molten", BlockBehaviour.Properties.of().noCollission().strength(100.0F).noLootTable()));
     
     public static final DeferredBlock<Block> FLUID_PROTODERMIS_PURE_MOLTEN = BLOCKS.registerSimpleBlock("fluid_protodermis_pure_molten",
-            BlockBehaviour.Properties.of().noCollission().strength(100.0F).noLootTable());
+            withBlockId("fluid_protodermis_pure_molten", BlockBehaviour.Properties.of().noCollission().strength(100.0F).noLootTable()));
 
     // Machines
     public static final DeferredBlock<Block> MASK_FORGE = BLOCKS.registerSimpleBlock("mask_forge",
-            BlockBehaviour.Properties.of().strength(3.5F).sound(SoundType.METAL));
+            withBlockId("mask_forge", BlockBehaviour.Properties.of().strength(3.5F).sound(SoundType.METAL)));
     
     public static final DeferredBlock<Block> PURIFIER = BLOCKS.registerSimpleBlock("purifier",
-            BlockBehaviour.Properties.of().strength(3.5F).sound(SoundType.METAL));
+            withBlockId("purifier", BlockBehaviour.Properties.of().strength(3.5F).sound(SoundType.METAL)));
 
     // Decorative blocks
     public static final DeferredBlock<BlockKoro> KORO_BLOCK = BLOCKS.registerBlock("koro_block",
-            BlockKoro::new, BlockKoro.createProperties());
+            BlockKoro::new, withBlockId("koro_block", BlockKoro.createProperties()));
 
     // Special blocks
     public static final DeferredBlock<BlockNuvaCube> NUVA_CUBE = BLOCKS.registerBlock("nuva_cube",
-            BlockNuvaCube::new, BlockNuvaCube.createProperties());
+            BlockNuvaCube::new, withBlockId("nuva_cube", BlockNuvaCube.createProperties()));
     
     public static final DeferredBlock<BlockBionicleStone> MATANUI_STONE = BLOCKS.registerBlock("matanui_stone",
-            BlockBionicleStone::new, BlockBionicleStone.createProperties());
+            BlockBionicleStone::new, withBlockId("matanui_stone", BlockBionicleStone.createProperties()));
     
     public static final DeferredBlock<BlockBionicleStone> MAKUTA_STONE = BLOCKS.registerBlock("makuta_stone",
-            BlockBionicleStone::new, BlockBionicleStone.createProperties());
+            BlockBionicleStone::new, withBlockId("makuta_stone", BlockBionicleStone.createProperties()));
     
     // Lightstone
     public static final DeferredBlock<BlockLightstone> LIGHTSTONE = BLOCKS.registerBlock("lightstone",
-            BlockLightstone::new, BlockLightstone.createProperties());
+            BlockLightstone::new, withBlockId("lightstone", BlockLightstone.createProperties()));
 
     // Ores
     public static final DeferredBlock<BlockOre> LIGHTSTONE_ORE = BLOCKS.registerBlock("lightstone_ore",
-            BlockOre::new, BlockOre.createProperties().lightLevel((state) -> 10));
+            BlockOre::new, withBlockId("lightstone_ore", BlockOre.createProperties().lightLevel((state) -> 10)));
     
     public static final DeferredBlock<BlockOre> HEATSTONE_ORE = BLOCKS.registerBlock("heatstone_ore",
-            BlockOre::new, BlockOre.createProperties());
+            BlockOre::new, withBlockId("heatstone_ore", BlockOre.createProperties()));
     
     public static final DeferredBlock<BlockProtodermisDeposit> PROTODERMIS_ORE = BLOCKS.registerBlock("protodermis_ore",
-            BlockProtodermisDeposit::new, BlockOre.createProperties());
+            BlockProtodermisDeposit::new, withBlockId("protodermis_ore", BlockOre.createProperties()));
 
     public static final DeferredBlock<BlockOre> ONU_WAHI_STONE_ORE = BLOCKS.registerBlock("onu_wahi_stone_ore",
-            BlockOre::new, BlockOre.createProperties());
+            BlockOre::new, withBlockId("onu_wahi_stone_ore", BlockOre.createProperties()));
 
     // Plants
     public static final DeferredBlock<BlockBamboo> BAMBOO = BLOCKS.registerBlock("bamboo",
-            BlockBamboo::new, BlockBamboo.createProperties());
+            BlockBamboo::new, withBlockId("bamboo", BlockBamboo.createProperties()));
 
     // Metal blocks
     public static final DeferredBlock<BlockMetal> BLOCK_PROTODERMIS = BLOCKS.registerBlock("block_protodermis",
-            BlockMetal::new, BlockMetal.createProperties());
+            BlockMetal::new, withBlockId("block_protodermis", BlockMetal.createProperties()));
     
     public static final DeferredBlock<BlockMetal> BLOCK_PROTOSTEEL = BLOCKS.registerBlock("block_protosteel",
-            BlockMetal::new, BlockMetal.createProperties());
+            BlockMetal::new, withBlockId("block_protosteel", BlockMetal.createProperties()));
 
     // Koro stones (for structures / decoration)
     public static final DeferredBlock<Block> TA_KORO_STONE = BLOCKS.registerSimpleBlock("ta_koro_stone",
-            BlockBehaviour.Properties.of().strength(2.0F).sound(SoundType.STONE));
+            withBlockId("ta_koro_stone", BlockBehaviour.Properties.of().strength(2.0F).sound(SoundType.STONE)));
     public static final DeferredBlock<Block> ONU_KORO_STONE = BLOCKS.registerSimpleBlock("onu_koro_stone",
-            BlockBehaviour.Properties.of().strength(2.0F).sound(SoundType.STONE));
+            withBlockId("onu_koro_stone", BlockBehaviour.Properties.of().strength(2.0F).sound(SoundType.STONE)));
     public static final DeferredBlock<Block> GA_KORO_STONE = BLOCKS.registerSimpleBlock("ga_koro_stone",
-            BlockBehaviour.Properties.of().strength(2.0F).sound(SoundType.STONE));
+            withBlockId("ga_koro_stone", BlockBehaviour.Properties.of().strength(2.0F).sound(SoundType.STONE)));
     public static final DeferredBlock<Block> KO_KORO_STONE = BLOCKS.registerSimpleBlock("ko_koro_stone",
-            BlockBehaviour.Properties.of().strength(2.0F).sound(SoundType.STONE));
+            withBlockId("ko_koro_stone", BlockBehaviour.Properties.of().strength(2.0F).sound(SoundType.STONE)));
     public static final DeferredBlock<Block> LE_KORO_STONE = BLOCKS.registerSimpleBlock("le_koro_stone",
-            BlockBehaviour.Properties.of().strength(2.0F).sound(SoundType.STONE));
+            withBlockId("le_koro_stone", BlockBehaviour.Properties.of().strength(2.0F).sound(SoundType.STONE)));
     public static final DeferredBlock<Block> PO_KORO_STONE = BLOCKS.registerSimpleBlock("po_koro_stone",
-            BlockBehaviour.Properties.of().strength(2.0F).sound(SoundType.STONE));
+            withBlockId("po_koro_stone", BlockBehaviour.Properties.of().strength(2.0F).sound(SoundType.STONE)));
 }
