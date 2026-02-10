ee38d7c0a95e87b4b8bd949ac218e6aa8411c701
mugenosantyago <147428965+mugenosantyago@users.noreply.github.com>
2026-02-10 11:10:58 -0600

    j

--- src/main/java/eastonium/nuicraft/core/NuiCraftEntityAttributes.java
+++ src/main/java/eastonium/nuicraft/core/NuiCraftEntityAttributes.java
@@ -8,59 +8,11 @@ public class NuiCraftEntityAttributes {
     
     @SubscribeEvent
     public static void registerEntityAttributes(EntityAttributeCreationEvent event) {
+        // Core Bionicle mobs with custom models
         event.put(NuiCraftEntityTypes.MAHI.get(), EntityMahi.createAttributes().build());
         event.put(NuiCraftEntityTypes.FIKOU.get(), EntityFikou.createAttributes().build());
         event.put(NuiCraftEntityTypes.HOI.get(), EntityHoi.createAttributes().build());
         event.put(NuiCraftEntityTypes.KOFO_JAGA.get(), EntityKofoJaga.createAttributes().build());
         event.put(NuiCraftEntityTypes.NUI_JAGA.get(), EntityNuiJaga.createAttributes().build());
-        
-        // Batch 1
-        event.put(NuiCraftEntityTypes.USSAL_CRAB.get(), EntityUssalCrab.createAttributes().build());
-        event.put(NuiCraftEntityTypes.SHEEP_RAHI.get(), EntitySheepRahi.createAttributes().build());
-        event.put(NuiCraftEntityTypes.RABBIT_RAHI.get(), EntityRabbitRahi.createAttributes().build());
-        
-        // Batch 2
-        event.put(NuiCraftEntityTypes.MUAKA.get(), EntityMuaka.createAttributes().build());
-        event.put(NuiCraftEntityTypes.KANE_RA.get(), EntityKaneRa.createAttributes().build());
-        event.put(NuiCraftEntityTypes.POKAWI.get(), EntityPokawi.createAttributes().build());
-        
-        // Batch 3
-        event.put(NuiCraftEntityTypes.TURTLE_RAHI.get(), EntityTurtleRahi.createAttributes().build());
-        event.put(NuiCraftEntityTypes.HIKAKI.get(), EntityHikaki.createAttributes().build());
-        event.put(NuiCraftEntityTypes.SHORE_TURTLE.get(), EntityShoreTurtle.createAttributes().build());
-        
-        // Batch 4
-        event.put(NuiCraftEntityTypes.BIRD_RAHI.get(), EntityBirdRahi.createAttributes().build());
-        event.put(NuiCraftEntityTypes.BAT_RAHI.get(), EntityBatRahi.createAttributes().build());
-        event.put(NuiCraftEntityTypes.HOTO_BUG.get(), EntityHotoBug.createAttributes().build());
-        
-        // Batch 5
-        event.put(NuiCraftEntityTypes.MATORAN.get(), EntityMatoran.createAttributes().build());
-        event.put(NuiCraftEntityTypes.BOHROK.get(), EntityBohrok.createAttributes().build());
-        event.put(NuiCraftEntityTypes.TURAGA.get(), EntityTuraga.createAttributes().build());
-        event.put(NuiCraftEntityTypes.MUKAU.get(), EntityMukau.createAttributes().build());
-        event.put(NuiCraftEntityTypes.HAPAKA.get(), EntityHapaka.createAttributes().build());
-        event.put(NuiCraftEntityTypes.BOHROK_VA.get(), EntityBohrokVa.createAttributes().build());
-        
-        // Batch 5b
-        event.put(NuiCraftEntityTypes.NUI_RAMA.get(), EntityNuiRama.createAttributes().build());
-        event.put(NuiCraftEntityTypes.ASH_BEAR.get(), EntityAshBear.createAttributes().build());
-        event.put(NuiCraftEntityTypes.RAHAGA.get(), EntityRahaga.createAttributes().build());
-        event.put(NuiCraftEntityTypes.VAKO.get(), EntityVako.createAttributes().build());
-        event.put(NuiCraftEntityTypes.KIKANALO.get(), EntityKikanalo.createAttributes().build());
-        event.put(NuiCraftEntityTypes.RAHKSHI.get(), EntityRahkshi.createAttributes().build());
-        
-        // Batch 5c
-        event.put(NuiCraftEntityTypes.KRAATA.get(), EntityKraata.createAttributes().build());
-        event.put(NuiCraftEntityTypes.PIRAKA.get(), EntityPiraka.createAttributes().build());
-        event.put(NuiCraftEntityTypes.DARK_HUNTER.get(), EntityDarkHunter.createAttributes().build());
-        event.put(NuiCraftEntityTypes.FUSA.get(), EntityFusa.createAttributes().build());
-        event.put(NuiCraftEntityTypes.SKRALL.get(), EntitySkrall.createAttributes().build());
-        event.put(NuiCraftEntityTypes.ZESK.get(), EntityZesk.createAttributes().build());
-        event.put(NuiCraftEntityTypes.AGORI.get(), EntityAgori.createAttributes().build());
-        event.put(NuiCraftEntityTypes.FOX_RAHI.get(), EntityFoxRahi.createAttributes().build());
-        event.put(NuiCraftEntityTypes.KAVINIKA.get(), EntityKavinika.createAttributes().build());
-        event.put(NuiCraftEntityTypes.ARCHIVES_MOLE.get(), EntityArchivesMole.createAttributes().build());
-        event.put(NuiCraftEntityTypes.TAKEA_SHARK.get(), EntityTakeaShark.createAttributes().build());
     }
 }
--- src/main/java/eastonium/nuicraft/core/NuiCraftSpawnPlacements.java
+++ src/main/java/eastonium/nuicraft/core/NuiCraftSpawnPlacements.java
@@ -13,7 +13,7 @@ import net.neoforged.neoforge.event.entity.RegisterSpawnPlacementsEvent;
 public class NuiCraftSpawnPlacements {
 
     public static void register(RegisterSpawnPlacementsEvent event) {
-        // Original 5 mobs
+        // Core Bionicle mobs
         event.register(NuiCraftEntityTypes.MAHI.get(),
                 SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
                 Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
@@ -29,124 +29,5 @@ public class NuiCraftSpawnPlacements {
         event.register(NuiCraftEntityTypes.NUI_JAGA.get(),
                 SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
                 Monster::checkMonsterSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        
-        // Batch 1
-        event.register(NuiCraftEntityTypes.USSAL_CRAB.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Monster::checkMonsterSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        event.register(NuiCraftEntityTypes.SHEEP_RAHI.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        event.register(NuiCraftEntityTypes.RABBIT_RAHI.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        
-        // Batch 2
-        event.register(NuiCraftEntityTypes.MUAKA.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        event.register(NuiCraftEntityTypes.KANE_RA.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        event.register(NuiCraftEntityTypes.POKAWI.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        
-        // Batch 3
-        event.register(NuiCraftEntityTypes.TURTLE_RAHI.get(),
-                SpawnPlacementTypes.IN_WATER, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        event.register(NuiCraftEntityTypes.HIKAKI.get(),
-                SpawnPlacementTypes.IN_WATER, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        event.register(NuiCraftEntityTypes.SHORE_TURTLE.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        
-        // Batch 4 - Flying
-        event.register(NuiCraftEntityTypes.BIRD_RAHI.get(),
-                SpawnPlacementTypes.NO_RESTRICTIONS, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        event.register(NuiCraftEntityTypes.BAT_RAHI.get(),
-                SpawnPlacementTypes.NO_RESTRICTIONS, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        event.register(NuiCraftEntityTypes.HOTO_BUG.get(),
-                SpawnPlacementTypes.NO_RESTRICTIONS, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        
-        // Batch 5
-        event.register(NuiCraftEntityTypes.MATORAN.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        event.register(NuiCraftEntityTypes.BOHROK.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Monster::checkMonsterSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        event.register(NuiCraftEntityTypes.TURAGA.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        event.register(NuiCraftEntityTypes.MUKAU.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        event.register(NuiCraftEntityTypes.HAPAKA.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        event.register(NuiCraftEntityTypes.BOHROK_VA.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Monster::checkMonsterSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        
-        // Batch 5b
-        event.register(NuiCraftEntityTypes.NUI_RAMA.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        event.register(NuiCraftEntityTypes.ASH_BEAR.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        event.register(NuiCraftEntityTypes.RAHAGA.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        event.register(NuiCraftEntityTypes.VAKO.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        event.register(NuiCraftEntityTypes.KIKANALO.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        event.register(NuiCraftEntityTypes.RAHKSHI.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Monster::checkMonsterSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        
-        // Batch 5c
-        event.register(NuiCraftEntityTypes.KRAATA.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        event.register(NuiCraftEntityTypes.PIRAKA.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        event.register(NuiCraftEntityTypes.DARK_HUNTER.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        event.register(NuiCraftEntityTypes.FUSA.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        event.register(NuiCraftEntityTypes.SKRALL.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        event.register(NuiCraftEntityTypes.ZESK.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        event.register(NuiCraftEntityTypes.AGORI.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        event.register(NuiCraftEntityTypes.FOX_RAHI.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        event.register(NuiCraftEntityTypes.KAVINIKA.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        event.register(NuiCraftEntityTypes.ARCHIVES_MOLE.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-        event.register(NuiCraftEntityTypes.TAKEA_SHARK.get(),
-                SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
     }
 }
