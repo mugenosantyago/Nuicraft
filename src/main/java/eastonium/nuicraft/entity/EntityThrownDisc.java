0ad7dcf719ca652f5246ce6797f58755b7da4ab7
mugenosantyago <147428965+mugenosantyago@users.noreply.github.com>
2026-02-16 14:42:06 -0600

    ,

--- src/main/java/eastonium/nuicraft/entity/EntityThrownDisc.java
+++ src/main/java/eastonium/nuicraft/entity/EntityThrownDisc.java
@@ -3,15 +3,20 @@ package eastonium.nuicraft.entity;
 import eastonium.nuicraft.core.NuiCraftEntityTypes;
 import eastonium.nuicraft.core.NuiCraftItems;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.HitResult;
 
 /**
  * Projectile entity for throwable Kanoka/bamboo discs.
  * Renders as the item that was thrown.
+ * On impact, the disc returns to the thrower (or drops at impact if no owner).
  */
 public class EntityThrownDisc extends ThrowableItemProjectile {
 
@@ -26,5 +31,32 @@ public class EntityThrownDisc extends ThrowableItemProjectile {
     @Override
     protected Item getDefaultItem() {
         return NuiCraftItems.KANOKA_DISC.get();
+    }
+
+    @Override
+    protected void onHit(HitResult hitResult) {
+        super.onHit(hitResult);
+        if (!this.level().isClientSide) {
+            ItemStack stack = this.getItem();
+            if (!stack.isEmpty()) {
+                LivingEntity owner = getOwner() instanceof LivingEntity living ? living : null;
+                if (owner != null) {
+                    if (owner instanceof Player player) {
+                        if (!player.getInventory().add(stack)) {
+                            player.drop(stack, false);
+                        }
+                    } else if (owner.getMainHandItem().isEmpty()) {
+                        owner.setItemSlot(EquipmentSlot.MAINHAND, stack);
+                    } else {
+                        ItemEntity drop = new ItemEntity(level(), owner.getX(), owner.getY(), owner.getZ(), stack);
+                        level().addFreshEntity(drop);
+                    }
+                } else {
+                    ItemEntity drop = new ItemEntity(level(), getX(), getY(), getZ(), stack);
+                    level().addFreshEntity(drop);
+                }
+            }
+            this.discard();
+        }
     }
 }
