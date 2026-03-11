638ef90468390d80ec4b7875b37ed98e90e33e87
mugenosantyago <147428965+mugenosantyago@users.noreply.github.com>
2026-03-11 18:33:40 -0500

    k

--- src/main/java/eastonium/mnogii/entity/EntityNuiRama.java
+++ src/main/java/eastonium/mnogii/entity/EntityNuiRama.java
@@ -163,7 +163,7 @@ public class EntityNuiRama extends TamableAnimal {
         super.tick();
         if (!this.level().isClientSide) {
             boolean moving = this.getDeltaMovement().lengthSqr() > 1.0E-5;
-            if (moving != lastMoving || this.tickCount == 1) {
+            if (moving != lastMoving || this.tickCount % 20 == 1) {
                 lastMoving = moving;
                 NuiRamaAnimator.sendMovementCommand(this);
             }
