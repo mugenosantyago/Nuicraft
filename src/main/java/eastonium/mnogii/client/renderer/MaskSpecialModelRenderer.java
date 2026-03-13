cd2586b20a36ed7201c0b2900321923b65b5e5d4
mugenosantyago <147428965+mugenosantyago@users.noreply.github.com>
2026-03-12 20:39:06 -0500

    g

--- src/main/java/eastonium/mnogii/client/renderer/MaskSpecialModelRenderer.java
+++ src/main/java/eastonium/mnogii/client/renderer/MaskSpecialModelRenderer.java
@@ -79,19 +79,11 @@ public class MaskSpecialModelRenderer implements SpecialModelRenderer<Void> {
 
         poseStack.pushPose();
         float s = DEFAULT_SCALE * scale;
-        if (context == ItemDisplayContext.GUI) {
-            poseStack.translate(0.5, 0.0, 0);
-            poseStack.scale(s, s, s);
-            if (flipY) {
-                poseStack.mulPose(new Quaternionf().rotationY((float) Math.PI));
-            }
-        } else {
-            poseStack.translate(offsetX, 0, 0);
-            poseStack.scale(s, s, s);
-            poseStack.translate(centerX / scale, centerY / scale, centerZ / scale);
-            if (flipY) {
-                poseStack.mulPose(new Quaternionf().rotationY((float) Math.PI));
-            }
+        poseStack.translate(offsetX, 0, 0);
+        poseStack.scale(s, s, s);
+        poseStack.translate(centerX / scale, centerY / scale, centerZ / scale);
+        if (flipY) {
+            poseStack.mulPose(new Quaternionf().rotationY((float) Math.PI));
         }
 
         for (AzBone bone : model.getTopLevelBones()) {
